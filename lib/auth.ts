91c47c6ce44e9b8de14a8e185b1d3ee12e86f143
soham2k06 <sohmm.dev@gmail.com>
2026-07-13 17:14:21 +0530

    fix: add signup rate limit

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,17 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+  rateLimit: {
+    storage: "memory",
+    window: 60 * 1000, // 1 minute
+    max: 10, // 10 requests per window
+    customRules: {
+      "/sign-up/email": {
+        window: 60 * 1000, // 1 minute
+        max: 1, // 1 request per window
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
